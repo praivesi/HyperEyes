33c0cd9bcc2c1da3486ef95a1b82137856bed1ef
praivesi <praivesi@gmail.com>
2021-03-04 21:50:22 +0900

    Rename solution name and project names & Add referencing to FFMPEG in HyperRenderer's main

---
+++ HyperRenderer/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+
+int main()
+{
+	AVCodec* av_codec;
+
+	_getch();
+}
